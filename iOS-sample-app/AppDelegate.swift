c0f6fe8112742221115cfb29a3a5db7a1ff731e4
Onur Yılmaz <onur@10.0.2.15>
2022-03-15 15:41:00 +0300

    Add sample carthage app

---
+++ iOS-sample-app/AppDelegate.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Note that you should not manually start the Contentsquare SDK, it autostarts by itself
+        return true
+    }
+}
+
